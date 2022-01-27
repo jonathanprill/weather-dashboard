f81a3d419b402cfaa1eb6fd2bcca627f4a1821e6
jonyprill <jonathantprill@gmail.com>
2022-01-27 17:17:18 -0600

    Added date and city onto screen dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,33 +7,124 @@
 
 
 
+/////////////////////////////////////
 
 
-
-
-
-
-var cityName = "Richmond"
+// var cityName = "Richmond"
 
 //getting api for city to lat long conversion
-var Coords = ("https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=6ba36869088d690b197fddb2f2b348d2");
+// var ApiCoords = ("https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=6ba36869088d690b197fddb2f2b348d2");
 
 //returned JSON to get Lat and Lon
 
-fetch(Coords).then(function(response) {
+// fetch(ApiCoords).then(function(response) {
     
-    response.json().then(function(data) {
-        //console.log(data[0].lat);
-        console.log("https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&appid=6ba36869088d690b197fddb2f2b348d2")
+//     response.json().then(function(data) {
+//         //console.log(data[0].lat);
+//         console.log("https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&appid=6ba36869088d690b197fddb2f2b348d2")
+//     });
+    
+// });
+
+
+/////////////////////////////////////
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#city-input");
+var weatherSpecsEl = document.querySelector("#weather-specs");
+var locationEl = document.querySelector("#location");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    
+    //gets value from input element
+    var city = cityInputEl.value.trim();
+
+    if(city) {
+        getCityCoords(city);
+        cityInputEl.value="";
+        weatherSpecsEl.textContent="";
+        getCityName(city);
+    } else {
+        alert("Please enter a City");
+    }
+  };
+
+
+
+cityFormEl.addEventListener("submit", formSubmitHandler);
+
+
+
+var getCityCoords = function(city) {
+    //format the github api url
+    var ApiCoords = ("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=6ba36869088d690b197fddb2f2b348d2");
+
+    //make a request to the url
+    fetch(ApiCoords).then(function(response) {
+        if (response.ok) {
+        response.json().then(function(data) {
+            var selectedCity = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&appid=6ba36869088d690b197fddb2f2b348d2");
+            displayWeather(selectedCity);
+            // console.log(ApiCoords)
+        });
+        } else {
+            alert("Error: City Not Found")
+        } 
+       
+    })
+    .catch(function(error) {
+        //notice this `.catch()` getting chained onto the end of the ` .then()` method
+        alert("Unable to connect to openweathermap.org");
+    });
+};
+
+
+
+
+
+
+var displayWeather = function(selectedCity) {
+     
+    fetch(selectedCity).then(function(response) {
+    
+            response.json().then(function(data) {
+                //console.log(data);
+
+            });
+            
     });
     
 
-});
+} ;
 
 
 
 
 
-//logging weather data for searched city
-//console.log("https://api.openweathermap.org/data/2.5/onecall?lat=" + Coords.lat + "&lon=" + Coords.lon + "&appid=6ba36869088d690b197fddb2f2b348d2")
 
+
+
+  // var currentWeatherEl = document.createElement("p");
+    // currentWeatherEl.textContent = selectedCity
+    // weatherSpecsEl.appendChild(currentWeatherEl);
+
+
+
+    var getCityName = function(city) {
+        //format the github api url
+        var cityName = ("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=6ba36869088d690b197fddb2f2b348d2");
+    
+        //make a request to the url
+        fetch(cityName).then(function(response) {
+            response.json().then(function(data) {
+
+                
+            console.log(data)
+            var currentWeatherEl = document.createElement("h3");
+            currentWeatherEl.textContent = data[0].name + " (" +moment().subtract(10, 'days').calendar() +")"
+            weatherSpecsEl.append(currentWeatherEl);
+            });
+        })
+    };
+
+    
